7f3ff9a74c7f01aa71ad65baa010b75a13c6a0d5
MAXOYT <valov.maks.2008@gmail.com>
2025-11-16 20:02:26 +0700

    drfgdfg

--- app/src/main/java/com/example/pz7valovmaximpr_23101/main.java
+++ app/src/main/java/com/example/pz7valovmaximpr_23101/main.java
@@ -1,9 +1,6 @@
 package com.example.pz7valovmaximpr_23101;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.register);
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Создание явного Intent
-                Intent intent = new Intent(MainActivity.this, main.class);
-                // Запуск новой активности
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
 }
