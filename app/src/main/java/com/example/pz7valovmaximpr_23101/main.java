a9a49010e2daebc54bfa1a03993c1a1889c4a73e
MAXOYT <valov.maks.2008@gmail.com>
2025-11-16 21:45:54 +0700

    last

--- app/src/main/java/com/example/pz7valovmaximpr_23101/main.java
+++ app/src/main/java/com/example/pz7valovmaximpr_23101/main.java
@@ -1,6 +1,9 @@
 package com.example.pz7valovmaximpr_23101;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class main extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main.this, users.class);
+                startActivity(intent);
+            }
+        };
+
+        findViewById(R.id.button1).setOnClickListener(buttonClickListener);
+        findViewById(R.id.button2).setOnClickListener(buttonClickListener);
+        findViewById(R.id.button3).setOnClickListener(buttonClickListener);
+        findViewById(R.id.button4).setOnClickListener(buttonClickListener);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
